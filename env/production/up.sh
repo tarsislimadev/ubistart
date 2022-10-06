c1f02f477ecbac876689e4b4a3ad23e2d707de1d
Tarsis de Lima <br.tmvdl@gmail.com>
2022-10-06 15:28:40 -0300

    Updating environment scripts

--- env/production/up.sh
+++ env/production/up.sh
@@ -1,3 +1,3 @@
-docker-compose -f ./docker-compose.yaml run app npm ci
+sudo docker-compose -f ./docker-compose.yaml run app npm ci
 
-docker-compose -f ./docker-compose.yaml up -d --remove-orphans --force-recreate --build 
+sudo docker-compose -f ./docker-compose.yaml up -d --remove-orphans --force-recreate --build 
